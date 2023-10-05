0a774c14c3010dda3ce3274a7444ea499c8f2293
Dorin <gimiri99@gmail.com>
2023-10-04 22:55:02 -0400

    Finished the logic of the game

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
 function getComputerChoice(){
-    let choice = Math.floor(Math.random() * 3);
-    console.log(choice);
-    return choice;
+    const choices = ["Rock", "Paper", "Scissor"];
+    let index = Math.floor(Math.random() * 3);
+    return choices[index];
 }
 
-console.log(getComputerChoice())
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.charAt(0).toUpperCase() + playerSelection.slice(1).toLowerCase();
+
+     if (playerSelection === "Rock" && computerSelection === "Paper"){
+        return "You Lose!" + computerSelection + " beats " + playerSelection;
+    } else if (computerSelection === "Rock" && playerSelection === "Paper"){
+        return "You win! Paper beats Rock"
+    } else if (playerSelection === "Scissor" && computerSelection === "Rock"){
+        return "You Lose!" + computerSelection + " beats " + playerSelection;
+    } else if (computerSelection === "Scissor" && playerSelection === "Rock"){
+        return "You win! Rock beats Scissor"
+    }else if (playerSelection === "Paper" && computerSelection === "Scissor"){
+        return "You Lose!" + computerSelection + " beats " + playerSelection;
+    } else if(computerSelection === "Paper" && playerSelection === "Scissor"){
+        return "You win! Scissor beats Paper"
+    } else{
+        return "It's a tie!"
+    }
+
+}
+
+function game(){
+    for (let i = 0; i < 5; i++){
+       let playerSelection = prompt("Select rock paper or scissor");
+       const computerSelection = getComputerChoice();
+       console.log(playRound(playerSelection, computerSelection));
+    }
+}
+
+game();
